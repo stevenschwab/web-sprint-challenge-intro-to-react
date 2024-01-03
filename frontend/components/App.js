ead5fad058e39bb9e6d3942817d5653465d9d7cf
Steve Schwab <schwabs@umich.edu>
2024-01-03 10:32:43 -0800

    get data into one data structure

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPeople = 'http://localhost:9009/api/people';
+const urlPlanets = 'http://localhost:9009/api/planets';
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    const urls = [urlPeople, urlPlanets];
+
+    Promise.all(urls.map(url => axios.get(url)))
+      .then(responses => {
+        const combinedData = responses.map(response => response.data);
+        console.log(combinedData);
+        let rawPeople = combinedData[0]
+        let rawPlanets = combinedData[1]
+        const cleanedData = rawPeople.map(person => {
+          const rawHomeworld = rawPlanets.filter(planet => planet.id === person.homeworld)[0];
+          return { ...person, homeworld: rawHomeworld};
+        })
+        console.log(cleanedData);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, [])
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
